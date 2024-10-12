39234aa968c9750287365698bb08ce176cf7ce3a
luizamfsantos <luiza.marques_@hotmail.com>
2024-10-12 18:40:23 -0300

    preformat dataframes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import io
+from datetime import datetime
+import re
 from openpyxl import load_workbook
 
 def get_table_download_link(dfs):
@@ -10,6 +12,41 @@ def get_table_download_link(dfs):
             df.to_excel(writer, sheet_name=sheet_name, index=False)
     output.seek(0)
     return output
+
+from datetime import datetime
+
+def is_date(string):
+    date_patterns = [
+        '%d/%m/%Y', '%Y-%m-%d', '%d-%m-%Y', '%Y/%m/%d', '%d/%m/%y', '%y-%m-%d', 
+        '%y/%m/%d', '%d-%m-%y', '%m-%d-%Y', '%H:%M:%S', '%H:%M', '%I:%M %p', 
+        '%I:%M:%S %p', '%Y-%m-%d %H:%M:%S', '%Y-%m-%d %H:%M', '%Y-%m-%d %I:%M %p', 
+        '%Y-%m-%d %I:%M:%S %p', '%d-%m-%Y %H:%M:%S', '%d-%m-%Y %H:%M'
+    ]
+    
+    return any(
+        try_parse_date(string, pattern) for pattern in date_patterns
+    )
+
+def try_parse_date(string, pattern):
+    try:
+        datetime.strptime(string, pattern)
+        return True
+    except ValueError:
+        return False
+
+def hide_identity(df):
+    name_list = []
+    list_of_lists = df.to_dict('split')['data']
+    for l in list_of_lists:
+        name_list.extend(l)
+    name_list = list(set(name_list))
+    name_dict = {name: f'estudante_{i}' for i, name in enumerate(name_list)}
+    # remove from dict time strings
+    for name in name_list:
+        if is_date(name):
+            name_dict.pop(name)
+    df = df.replace(name_dict)
+    return df
 
 st.title('Ocultar Identidade')
 
@@ -22,6 +59,8 @@ if uploaded_files:
         for sheet_name in xls.sheet_names:
             df = pd.read_excel(xls, sheet_name=sheet_name)
             df = df.astype(str)
+            df = df.replace('nan', '')
+            df = hide_identity(df)
             all_dfs[f"{uploaded_file.name} - {sheet_name}"] = df
 
     # Create tabs
